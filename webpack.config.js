f3a87f10f28c9b01706a422bc77d391634fb2f16
Thomas Kleiven <thomaskleiven@Thomass-MacBook-Pro.local>
2019-12-08 13:59:35 +0100

    index.html

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 
 module.exports = {
     entry: './js/email.js',
-    mode: 'development',
+    mode: 'production',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
